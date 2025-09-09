35fd643e9550d3ede33e64083ed9e3ad1796192f
cm <68245045+crm85@users.noreply.github.com>
2025-09-09 14:36:52 +0200

    jotting down some ideas

--- Plugin.cs
+++ Plugin.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BepInEx;
+using Unity;
+using UnityEngine;
 
 namespace ombarella
 {
-    public class Plugin
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
     {
+        const string modGUID = "Ombarella";
+        const string modName = "Ombarella";
+        const string modVersion = "0.1";
+
+        Camera _sampleCam;
+        Material _renderMat;
+        RenderTexture _renderTex;
+
+        void Awake()
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            _sampleCam = new Camera();
+            _renderMat = new Material(Shader.Find("Standard"));
+            _renderTex = new RenderTexture(200, 200, 5);
+            int nameID = _renderTex.GetInstanceID();
+            _renderMat.SetTexture(nameID, _renderTex);
+            /*
+             * Render Textures seem the way to go, 
+             * you can get a Camera to Render to it 
+             * then have a C# script go over the pixels
+             * and calculate either Value that is the 
+             * highest channel of the 3 or Luminosity 
+             * and that is something like (r * .2) + (g * .7) + (b * .1)
+             * and that will give you human perceived brightness.
+             */
+        }
     }
 }
