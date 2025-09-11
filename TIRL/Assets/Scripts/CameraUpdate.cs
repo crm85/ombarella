9e262ee3bc00a6f8b00a6ee4d7cfffbdcaaeb75b
cm <68245045+crm85@users.noreply.github.com>
2025-09-11 13:02:33 +0200

    moved unity project to here

---
+++ TIRL/Assets/Scripts/CameraUpdate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraUpdate : MonoBehaviour
+{
+    [SerializeField] Camera _lightCamera;
+    [SerializeField] Transform _playerProp;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
